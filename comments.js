38e3b1b8ea45aa6384f48a5473d7c068107c1872
Deepak Singh <deepak.singh.buzz@gmail.com>
2024-12-13 14:44:21 +0000

    Fix commentjs

--- comments.js
+++ comments.js
@@ -1,27 +1,43 @@
-//create web server
-const express = require('express');
-const app = express();
+//create simple web server 
+//run on port 8080
+//use express to handle requests
+//use body-parser to parse request body
+//use mongoose to interact with mongodb
+//use comments.js to handle comments
+//use comments.html as the default page
+//use comments.css for styling
+//use comments.js for client side scripting
+//use comment.js as the model for comments
+//use comments as the collection in mongodb
 
-//get request
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Comment = require('./comment');
+
+var app = express();
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/comments');
+
+app.get('/comments', function(req, res) {
+    Comment.find(function(err, comments) {
+        res.send(comments);
+    });
 });
 
-//post request
-app.post('/', (req, res) => {
-    res.send('Got a POST request');
+app.post('/comments', function(req, res) {
+    var comment = new Comment(req.body);
+    comment.save(function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(200);
+        }
+    });
 });
 
-//put request
-app.put('/user', (req, res) => {
-    res.send('Got a PUT request at /user');
-});
-
-//delete request
-app.delete('/user', (req, res) => {
-    res.send('Got a DELETE request at /user');
-});
-
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
-});
+app.listen(8080);
+console.log('Server running on port 8080');
